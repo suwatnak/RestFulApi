068dcf4297ca92c13d3a6d1d87678525d68dbd22
suwat <suwat@suwats-MacBook-Pro.local>
2017-06-21 22:42:33 +0700

    exprss  install

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express();
+const PORT = 3000
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+  res.send('Hello World')
+})
+
+app.listen(PORT ,() => {
+  console.log("App listening on http://localhost:" + PORT);
+})
