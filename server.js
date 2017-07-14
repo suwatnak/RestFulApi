acc2bb55e303978c630bbad4935b5164a7ef60f5
SUWAT <suwat@thinknet.co.th>
2017-07-14 19:17:40 +0700

    แยกออกไปยัง server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+export function setup() {
+  const app = express()
+  const PORT = 3000
+
+  app.use(bodyParser.urlencoded({ extended: true}))
+  app.use(bodyParser.json())
+
+  app.get('/', (req, res)=> {
+    res.send('Hello World')
+  })
+
+  app.listen(PORT, ()=>{
+      console.log('Running on http://localhost:' + PORT);
+  })
+}
