fb548e7bdcea2055dd3447a01d7d8d641984b8d6
SUWAT <suwat@thinknet.co.th>
2017-07-14 19:29:12 +0700

    db.js

---
+++ db.js
@@ -0,0 +1,42 @@
+export default {
+  articles: [
+    {
+      id:1,
+      title: 'Introduction to JavaScript',
+      content: 'Lorem Ipsum',
+      authorId: 1
+    },
+    {
+      id: 2,
+      title: 'Introduction to Python',
+      content: 'Lorem Ipsum',
+      authorId: 1
+    },
+    {
+      id: 3,
+      title: 'Introduction to Elixir',
+      content: 'Lorem Ipsum',
+      authorId: 2
+    },
+    {
+      id: 4,
+      title: 'Introduction to Ruby',
+      content: 'Lorem Ipsum',
+      authorId: 2
+    },
+  ],
+  users:[
+    {
+      id: 1,
+      email: 'suwat@gmail.com',
+      isAdmin: true,
+      password: ''
+    },
+    {
+      id: 2,
+      email: 'suwat@gmail.com',
+      isAdmin: false,
+      password:''
+    }
+  ]
+}
